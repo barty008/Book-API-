588766f59dcf4592d313cf7ce386ce6d7fd52ce0
James <jamesb88@hotmail.co.uk>
2023-02-14 17:12:58 +0000

    14/02/23 all working

--- utlities.js
+++ utlities.js
@@ -2,7 +2,7 @@
 
 const stars = document.querySelectorAll(".fa-star");
 
-const allStars = stars.forEach((star, index) => {
+stars.forEach((star, index) => {
   star.addEventListener("click", () => {
     stars.forEach((otherStar, otherIdx) => {
       if (otherIdx <= index) {
